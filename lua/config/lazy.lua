f691666dccbf6bd4a930aa5322e664b028abbaf3
Marcell Munnich <marcellmunnich@pm.me>
2025-02-26 22:09:26 +0100

    split example plugins into separate files + added nord theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,4 +50,7 @@ require("lazy").setup({
       },
     },
   },
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
 })
