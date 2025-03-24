fce370eb2074948f4d4353756be004c1e928c27b
Marcell Munnich <marcellmunnich@pm.me>
2025-03-24 18:29:45 +0100

    added lsp file operations plugin + updated plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -122,4 +122,17 @@ return {
       })
     end,
   },
+
+  {
+    {
+      "antosha417/nvim-lsp-file-operations",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-neo-tree/neo-tree.nvim",
+      },
+      config = function()
+        require("lsp-file-operations").setup()
+      end,
+    },
+  },
 }
