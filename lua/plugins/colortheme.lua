7a08599be908ff1a072584c8a939ad5db0a9b75c
Marcell Munnich <marcellmunnich@pm.me>
2025-03-26 18:48:37 +0100

    changed colortheme + added transparency toggle

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,9 +1,34 @@
+-- Nord
+-- return {
+--   "gbprod/nord.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("nord").setup({})
+--     vim.cmd.colorscheme("nord")
+--   end,
+-- }
+
+-- Solarized
+--
+local isTransparent = false
+local function toggleTransparent()
+  isTransparent = not isTransparent
+  require("solarized").setup({ transparent = { enabled = isTransparent } })
+  vim.cmd.colorscheme("solarized")
+end
+
 return {
-  "gbprod/nord.nvim",
+  "maxmx01/solarized.nvim",
   lazy = false,
-  priority = 1000,
-  config = function()
-    require("nord").setup({})
-    vim.cmd.colorscheme("nord")
+  priority = 998,
+  ---@type solarized.config
+  opts = { transparent = { enabled = isTransparent } },
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+    require("solarized").setup(opts)
+    vim.cmd.colorscheme("solarized")
+    vim.keymap.set("n", "<leader>t", toggleTransparent, { desc = "Toggle transparent background" })
   end,
 }
--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,1 +1,3 @@
-return { "folke/flash.nvim", enabled = false }
+return {
+  { "folke/flash.nvim", enabled = false },
+}
