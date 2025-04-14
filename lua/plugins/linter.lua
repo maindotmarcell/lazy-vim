035b620c9a233f6e35785f6619a49ddee5699dea
marcell.muennich <marcell.muennich@bitpanda.com>
2025-04-14 14:14:38 +0200

    added tslint support

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -5,6 +5,38 @@ return {
   config = function()
     local lint = require("lint")
 
+    -- Define the tslint linter
+    lint.linters.tslint = {
+      cmd = "tslint",
+      args = { "--format", "json" },
+      ignore_exitcode = true,
+      parser = function(output, bufnr)
+        local diagnostics = {}
+        if output == "" then
+          return diagnostics
+        end
+
+        local decoded = vim.json.decode(output)
+        if not decoded then
+          return diagnostics
+        end
+
+        for _, failure in ipairs(decoded) do
+          table.insert(diagnostics, {
+            lnum = failure.startPosition.line,
+            col = failure.startPosition.character,
+            end_lnum = failure.endPosition.line,
+            end_col = failure.endPosition.character,
+            message = failure.failure,
+            code = failure.ruleName,
+            severity = vim.diagnostic.severity.WARN,
+          })
+        end
+
+        return diagnostics
+      end,
+    }
+
     lint.linters_by_ft = {
       javascript = { "eslint" },
       typescript = { "eslint", "tslint" },
