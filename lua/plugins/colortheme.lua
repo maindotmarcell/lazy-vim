baba7aa5c4b9bd6f588aadb54717bd11b310446c
Marcell Munnich <marcellmunnich@pm.me>
2025-07-16 22:18:19 +0200

    switch to transparent bg

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,11 +1,34 @@
 -- Nord
+-- return {
+--   "gbprod/nord.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("nord").setup({})
+--     vim.cmd.colorscheme("nord")
+--   end,
+-- }
+
+--
+local isTransparent = false
+local function toggleTransparent()
+  isTransparent = not isTransparent
+  require("solarized").setup({ transparent = { enabled = isTransparent } })
+  vim.cmd.colorscheme("solarized")
+end
+
 return {
   "gbprod/nord.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    require("nord").setup({})
+  ---@type nord.config
+  opts = { transparent = { enabled = isTransparent } },
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+    require("nord").setup(opts)
     vim.cmd.colorscheme("nord")
+    vim.keymap.set("n", "<leader>t", toggleTransparent, { desc = "Toggle transparent background" })
   end,
 }
 
